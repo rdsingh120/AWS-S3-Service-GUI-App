92fb66de4924f64152bda43fa40f9fd09cd3eef9
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-18 04:37:21 +0530

    created object level ops window

--- AWSS3Service/MainWindow.xaml.cs
+++ AWSS3Service/MainWindow.xaml.cs
@@ -17,7 +17,8 @@ namespace AWSS3Service
 
         private void ObjectOpsButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ObjectLevelOperations objectLevelOperations = new ObjectLevelOperations();  
+            objectLevelOperations.ShowDialog();
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
---
+++ AWSS3Service/ObjectLevelOperations.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AWSS3Service
+{
+    /// <summary>
+    /// Interaction logic for ObjectLevelOperations.xaml
+    /// </summary>
+    public partial class ObjectLevelOperations : Window
+    {
+        public ObjectLevelOperations()
+        {
+            InitializeComponent();
+        }
+    }
+}
