319d4ae8327681bdc6b0a445f3c718c314583cdc
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-18 15:38:43 +0530

    create bucket function connected with gui

--- AWSS3Service/Views/BucketLevelOperations.xaml.cs
+++ AWSS3Service/Views/BucketLevelOperations.xaml.cs
@@ -1,17 +1,45 @@
-﻿using System.Windows;
+﻿using AWSS3Service.Services;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace AWSS3Service
 {
     public partial class BucketLevelOperations : Window
     {
+        BucketOperations bucketOperations;
         public BucketLevelOperations()
         {
             InitializeComponent();
+            this.bucketOperations = new BucketOperations();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private async void CreateBucketButton_Click(object sender, RoutedEventArgs e)
+        {
+            string bucketName = BucketNameTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(bucketName)) 
+            {
+                MessageBox.Show("Please enter the bucket name");
+            }
+            else
+            {
+                try
+                {
+                    await bucketOperations.CreateBucket(bucketName);
+                    MessageBox.Show($"Bucket {bucketName} created successfully.");
+                }
+                catch( Exception ex )
+                {
+                    MessageBox.Show($"Error creating bucket: {ex.Message}");
+                }
+
+                BucketNameTextBox.Text = string.Empty;
+            }
         }
     }
 }
