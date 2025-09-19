847a917c102c2d7ede9f815e39c0b23f627a76a8
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-19 10:29:14 +0530

    created userControl for create bucket section in bucket level ops window

---
+++ AWSS3Service/UserControls/CreateBucketSection.xaml.cs
@@ -0,0 +1,41 @@
+﻿using AWSS3Service.Services;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AWSS3Service.UserControls
+{
+    public partial class CreateBucketSection : UserControl
+    {
+        public event Func<Task> BucketCreated;
+        public CreateBucketSection()
+        {
+            InitializeComponent();
+        }
+
+        private async void CreateBucketButton_Click(object sender, RoutedEventArgs e)
+        {
+            string bucketName = BucketNameTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(bucketName))
+            {
+                MessageBox.Show("Please enter the bucket name");
+            }
+            else
+            {
+                try
+                {
+                    await BucketOperations.CreateBucket(bucketName);
+                    MessageBox.Show($"Bucket {bucketName} created successfully.");
+                    BucketNameTextBox.Text = string.Empty;
+
+                    if (BucketCreated != null) await BucketCreated.Invoke();
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error creating bucket: {ex.Message}");
+                }                                
+            }
+        }
+    }
+}
--- AWSS3Service/Views/BucketLevelOperations.xaml.cs
+++ AWSS3Service/Views/BucketLevelOperations.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using AWSS3Service.Services;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace AWSS3Service
@@ -9,6 +8,7 @@ namespace AWSS3Service
         public BucketLevelOperations()
         {
             InitializeComponent();
+            CreateBucketSectionControl.BucketCreated += LoadBuckets;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -25,31 +25,6 @@ namespace AWSS3Service
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             await LoadBuckets();
-        }
-
-        private async void CreateBucketButton_Click(object sender, RoutedEventArgs e)
-        {
-            string bucketName = BucketNameTextBox.Text;
-
-            if (string.IsNullOrWhiteSpace(bucketName)) 
-            {
-                MessageBox.Show("Please enter the bucket name");
-            }
-            else
-            {
-                try
-                {
-                    await BucketOperations.CreateBucket(bucketName);
-                    MessageBox.Show($"Bucket {bucketName} created successfully.");
-                    await LoadBuckets();
-                }
-                catch( Exception ex )
-                {
-                    MessageBox.Show($"Error creating bucket: {ex.Message}");
-                }
-
-                BucketNameTextBox.Text = string.Empty;
-            }
         }
 
         private async void DeleteBucketButton_Click(object sender, RoutedEventArgs e)
