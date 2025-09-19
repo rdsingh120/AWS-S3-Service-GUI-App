5234428755fb6a19e12fdbf6e1f4a55cc385abee
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-19 10:46:30 +0530

    created userControl for view(datagrid) and delete section in bucket level ops

--- AWSS3Service/Views/BucketLevelOperations.xaml.cs
+++ AWSS3Service/Views/BucketLevelOperations.xaml.cs
@@ -8,7 +8,7 @@ namespace AWSS3Service
         public BucketLevelOperations()
         {
             InitializeComponent();
-            CreateBucketSectionControl.BucketCreated += LoadBuckets;
+            CreateBucketSectionControl.BucketCreated += ViewAndDeleteSectionControl.LoadBuckets;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -16,58 +16,6 @@ namespace AWSS3Service
             this.Close();
         }
 
-        private async Task LoadBuckets()
-        {
-            var buckets = await BucketOperations.GetBucketList();
-            BucketsDataGrid.ItemsSource = buckets;
-        }
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            await LoadBuckets();
-        }
-
-        private async void DeleteBucketButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (BucketsDataGrid.SelectedItem is not Amazon.S3.Model.S3Bucket selectedBucket)
-            {
-                MessageBox.Show("Please select a bucket first.");
-                return;
-            }
-
-            if(await BucketOperations.IsBucketEmpty(selectedBucket.BucketName))
-            {
-                try
-                {
-                    await BucketOperations.DeleteBucket(selectedBucket.BucketName);
-                    MessageBox.Show($"Bucket {selectedBucket.BucketName} deleted.");
-                    await LoadBuckets();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error deleting bucket: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBoxResult result = MessageBox.Show(
-                    "Bucket is not empty, still want to delete it?",
-                    $"{selectedBucket.BucketName} contains files",
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Warning);
-
-                if (result == MessageBoxResult.Yes) 
-                {
-                    bool isAllFilesDeleted = await BucketOperations.DeleteAllFiles(selectedBucket.BucketName);
-
-                    if (isAllFilesDeleted)
-                    {
-                        await BucketOperations.DeleteBucket(selectedBucket.BucketName);
-                        MessageBox.Show($"Bucket {selectedBucket.BucketName} deleted.");
-                        await LoadBuckets();
-                    }
-                }
-            }
-        }
+        
     }
 }
